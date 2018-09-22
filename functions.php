2db1e01a94ee9ff484ab5fe3ffeb375e4d41ff7b
balesniy <balesniy@yandex.ru>
2018-09-22 15:58:29 +0200

    дата время

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function include_template($name, $data) {
 
     ob_start();
     extract($data);
-    require_once $name;
+    require $name;
 
     $result = ob_get_clean();
 
@@ -19,5 +19,11 @@ function price_format($price){
     $price = ceil($price);
     $rub_format = $price>999 ? number_format($price, 0, ".", " ") : $price;
     return "$rub_format ₽";
+};
+function get_time(){
+    $current_time = date_create('now');
+    $lot_finish_time = date_create('tomorrow');
+    $interval = date_diff($lot_finish_time, $current_time);
+    return $interval->format('%H:%I');
 };
 ?>
--- templates/index.php
+++ templates/index.php
@@ -29,8 +29,8 @@
                                 <span class="lot__amount">Стартовая цена</span>
                                 <span class="lot__cost"><?=price_format($lot['price']) ?></span>
                             </div>
-                            <div class="lot__timer timer">666
-
+                            <div class="lot__timer timer">
+                                <?=get_time() ?>
                             </div>
                         </div>
                     </div>

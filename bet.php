28a3ea811742e1381d13ebadbfc487cbc77082f9
balesniy <balesniy@yandex.ru>
2018-10-15 12:39:52 +0200

    validate

--- bet.php
+++ bet.php
@@ -3,14 +3,28 @@
 require_once 'init.php';
 
 $errors = [];
-$lot = [];
+$required = ['cost', 'id'];
+$numbers = ['cost'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
+    foreach ($required as $key) {
+		if (empty($_POST[$key])) {
+            $errors[$key] = 'Это поле надо заполнить';
+		}
+    }
+
+    foreach ($numbers as $key) {
+		if (empty($errors[$key])) {
+            $errors = array_merge($errors, validate_number($_POST[$key], $key));
+		}
+    }
+
+
     $sql = "INSERT INTO bets (amount, user_id, lot_id) VALUES(?, ?, ?)";
         $stmt = db_get_prepare_stmt($link, $sql, [
-            $_POST['amount'], $user['id'], $_POST['id']
+            $_POST['cost'], $user['id'], $_POST['id']
         ]);
         $res = mysqli_stmt_execute($stmt);
 
